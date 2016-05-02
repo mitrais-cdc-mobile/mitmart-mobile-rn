fc383560e98a27fd19808b87968aaf61e812169a
Kadek Juliana Parwanta <kadekjuliana.parwanta@mitrais.com>
2016-05-02 14:19:12 +0800

    Add index and navigator for iOS

--- index.ios.js
+++ index.ios.js
@@ -5,49 +5,9 @@
  */
 
 import React, {
-  AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
+  AppRegistry
 } from 'react-native';
 
-class MitMartRN extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+import Navigators from './apps/modules/navigators/navigator';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('MitMartRN', () => MitMartRN);
+AppRegistry.registerComponent('MitMartRN', () => Navigators);
